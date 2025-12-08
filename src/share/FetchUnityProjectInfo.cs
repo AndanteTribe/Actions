31165527218107976fdc8301453d554b5309bc62
Masatake Iwasaki <78918084+kochounoyume@users.noreply.github.com>
2025-12-09 01:40:21 +0900

    ci: Implementation up to unity cache retrieval

---
+++ src/share/FetchUnityProjectInfo.cs
@@ -0,0 +1,19 @@
+using System;
+using System.IO;
+var projectPath = Environment.GetEnvironmentVariable("UNITY_PROJECT_PATH");
+if (string.IsNullOrEmpty(projectPath) || !Directory.Exists(projectPath))
+{
+    Console.Error.WriteLine("The UNITY_PROJECT_PATH environment variable is either not set or an invalid path.");
+    Environment.Exit(1);
+}
+
+var name = Path.GetFileName(projectPath.AsSpan());
+
+var output = Environment.GetEnvironmentVariable("GITHUB_OUTPUT");
+if (!string.IsNullOrEmpty(output))
+{
+    using var writer = File.AppendText(output);
+
+    writer.Write("name=");
+    writer.WriteLine(name);
+}
