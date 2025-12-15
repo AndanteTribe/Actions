bb495ccbe24bcddc0192fa26796e1cc7596a91fa
syogakusya <chitsuoman404@gmail.com>
2025-12-15 12:46:36 +0900

    fix: .Name残したままにしてしまっていたので修正

--- src/share/FetchUnityProjectInfo.cs
+++ src/share/FetchUnityProjectInfo.cs
@@ -8,7 +8,7 @@ if (string.IsNullOrEmpty(projectPath) || !Directory.Exists(projectPath))
 }
 
 var workspace = Environment.GetEnvironmentVariable("GITHUB_WORKSPACE");
-var name = Path.GetFileName(Path.Combine(workspace, projectPath)).Name;
+var name = Path.GetFileName(Path.Combine(workspace, projectPath));
 
 var output = Environment.GetEnvironmentVariable("GITHUB_OUTPUT");
 if (!string.IsNullOrEmpty(output))
