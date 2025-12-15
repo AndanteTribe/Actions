730a2ed0fe5b4b37cf73c68cea5f3ec7b1f98b71
syogakusya <chitsuoman404@gmail.com>
2025-12-15 10:31:32 +0900

    fix
    (cherry picked from commit d6a7660a645dba98330aa9238d121aadae4b53f6)

--- src/share/FetchUnityProjectInfo.cs
+++ src/share/FetchUnityProjectInfo.cs
@@ -7,7 +7,21 @@ if (string.IsNullOrEmpty(projectPath) || !Directory.Exists(projectPath))
     Environment.Exit(1);
 }
 
-var name = Path.GetFileName(Path.GetDirectoryName(projectPath.AsSpan()));
+var workspace = Environment.GetEnvironmentVariable("GITHUB_WORKSPACE");
+var combined =
+    Path.IsPathRooted(projectPathInput) || string.IsNullOrEmpty(workspace)
+        ? projectPathInput
+        : Path.Combine(workspace, projectPathInput);
+
+var projectPath = Path.GetFullPath(combined);
+
+if (!Directory.Exists(projectPath))
+{
+    Console.Error.WriteLine("The UNITY_PROJECT_PATH environment variable is either not set or an invalid path.");
+    Environment.Exit(1);
+}
+
+var name = new DirectoryInfo(projectPath).Name;
 
 var output = Environment.GetEnvironmentVariable("GITHUB_OUTPUT");
 if (!string.IsNullOrEmpty(output))
