0e6c79beb4f03cfc8d26eb2d9e2a9cb19b225bf1
syogakusya <chitsuoman404@gmail.com>
2025-12-15 12:37:01 +0900

    refactor: Unityプロジェクトのディレクトリ名を取得する方法を改善

--- src/share/FetchUnityProjectInfo.cs
+++ src/share/FetchUnityProjectInfo.cs
@@ -8,20 +8,8 @@ if (string.IsNullOrEmpty(projectPath) || !Directory.Exists(projectPath))
 }
 
 var workspace = Environment.GetEnvironmentVariable("GITHUB_WORKSPACE");
-var combined =
-    Path.IsPathRooted(projectPath) || string.IsNullOrEmpty(workspace)
-        ? projectPath
-        : Path.Combine(workspace, projectPath);
 
-projectPath = Path.GetFullPath(combined);
-
-if (!Directory.Exists(projectPath))
-{
-    Console.Error.WriteLine("The UNITY_PROJECT_PATH environment variable is either not set or an invalid path.");
-    Environment.Exit(1);
-}
-
-var name = new DirectoryInfo(projectPath).Name;
+var name = Path.GetFileName(Path.GetFullPath(projectPath));
 
 var output = Environment.GetEnvironmentVariable("GITHUB_OUTPUT");
 if (!string.IsNullOrEmpty(output))
