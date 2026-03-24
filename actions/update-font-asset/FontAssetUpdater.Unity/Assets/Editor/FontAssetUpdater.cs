191486250881770e3625b49c74b885b8c843e9d0
Masatake Iwasaki <78918084+kochounoyume@users.noreply.github.com>
2026-03-25 03:25:24 +0900

    feat: init unity project

---
+++ actions/update-font-asset/FontAssetUpdater.Unity/Assets/Editor/FontAssetUpdater.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+namespace AndanteTribe.TextMeshPro.Editor
+{
+    public static class FontAssetUpdater
+    {
+        public static void InsertCharacters()
+        {
+            var args = Environment.GetCommandLineArgs();
+        }
+    }
+}
