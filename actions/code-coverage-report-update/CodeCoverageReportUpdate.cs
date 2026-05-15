e0a6b70c156f283ce7fc301c2ee8328a18c1dfe6
syogakusya <Chitsuoman404@outlook.jp>
2026-05-15 19:19:43 +0900

    fix: coverage: summary pathをNullOrEmptyで弾くように変更

--- actions/code-coverage-report-update/CodeCoverageReportUpdate.cs
+++ actions/code-coverage-report-update/CodeCoverageReportUpdate.cs
@@ -1,6 +1,12 @@
 var root = args.Length > 0 ? args[0] : "CodeCoverage";
 var summaryPath = Environment.GetEnvironmentVariable("GITHUB_STEP_SUMMARY");
 
+if (!String.IsNullOrEmpty(summaryPath))
+{
+    Console.Error.WriteLine($"::error::No summary path specified: Environment.GetEnvironmentVariable(\"GITHUB_STEP_SUMMARY\")");
+    return;
+}
+
 if (!Directory.Exists(root))
 {
     Console.Error.WriteLine($"::warning::Directory not found: {root}");
